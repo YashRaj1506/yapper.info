982572c4e3853140928a4a7ef6fac6783bb0606a
YashRaj1506 <yashraj504300@gmail.com>
2025-03-25 14:04:26 +0530

    brought back db sqlite

--- yapper/yapper/settings.py
+++ yapper/yapper/settings.py
@@ -86,18 +86,10 @@ WSGI_APPLICATION = 'yapper.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.environ.get('DB_ENGINE'),
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
-DATABASE_URL = os.environ.get('DATABASE_URL')
-DATABASES["default"] = dj_database_url.parse(DATABASE_URL)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
