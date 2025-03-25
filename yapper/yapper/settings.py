683b93d259cef1ca46193ac00574cf48fad8ce0a
YashRaj1506 <yashraj504300@gmail.com>
2025-03-25 15:31:17 +0530

    .env.example and nginx for prod

--- yapper/yapper/settings.py
+++ yapper/yapper/settings.py
@@ -30,11 +30,11 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
-DEBUG = True
+DEBUG = False
 
 
 # ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '').split(',')
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS','').split(',')
 
 
 # Application definition
